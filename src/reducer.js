4a2636b8c25ca4136e88250dd198069f1811945b
Alexander2007-cyber <58745999+Alexander2007-cyber@users.noreply.github.com>
2021-04-22 20:31:11 +0300

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,6 @@
 export const initialState = {
-    basket: [],
     user: null
   };
-  
-  // Selector
-  export const getBasketTotal = (basket) => 
-    basket?.reduce((amount, item) => item.price + amount, 0);
   
   const reducer = (state, action) => {
     // console.log(action);
